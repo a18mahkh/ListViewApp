96869ab7320e71cb7395c413c05d3e5bd7519220
a18mahkh <ez42rmu8m>
2019-04-20 11:37:27 +0200

    Tagit bort list borders, lagt paddig, fontsize etc.

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,8 +2,17 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +20,55 @@ public class MainActivity extends AppCompatActivity {
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
+    List<String> listData=new ArrayList<String>(Arrays.asList(mountainNames));
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter=new ArrayAdapter(getApplicationContext(), R.layout.list_item_textview,
+                R.id.my_item_textview, listData);
+
+        ListView myListView = (ListView) findViewById(R.id.my_Listview);
+        myListView.setAdapter(adapter);
+        //adapter.add("hilding");
+        myListView.setDivider(null);
+
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                int bergHojd = mountainHeights[position];
+                Toast.makeText(getApplicationContext(), String.valueOf(bergHojd) + " Meter", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+
+        /* String[] kjell = {"Allamus", "David", "Anna"};
+
+
+
+        Toast.makeText(getApplicationContext(), Arrays.toString(kjell), Toast.LENGTH_SHORT).show();
+
+        Log.d("brom-debug", Arrays.toString(kjell));
+
+        Button b = (Button) findViewById(R.id.toasterButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText my_EdtBox = (EditText) findViewById(R.id.toasterText);
+                Toast.makeText(getApplicationContext(), my_EdtBox.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });/
 
         // The onCreate method is run when the app is created.
         // Before you can implement this you need to create the layout xml files that
@@ -30,6 +82,13 @@ public class MainActivity extends AppCompatActivity {
         // Here you should enter your code that fills the ListView
         // 1. Create an array
         // 2. Create a List object with your array from step 1 as in-data
+
+
+        /*adapter=new ArrayAdapter<String>(this, R.layout.list_item_textview, R.id.list_item_textview, listData);
+        ListView my_listview=(ListView) findViewById(R.id.my_listview);
+        my_listview.setAdapter(adapter); */
+
+
         // 3. Create an ArrayAdapter object that connects
         //    * list_item_textview
         //    * my_item_textview
